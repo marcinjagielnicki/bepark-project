e1a1009f923a143273cabd08c47eac20b157448c
Marcin Jagielnicki <marcin@ideacore.pl>
2021-02-02 02:38:37 +0100

    Styling fixes

--- resources/views/components/user-games.blade.php
+++ resources/views/components/user-games.blade.php
@@ -3,13 +3,12 @@
 <div>
     @if($user->games()->count())
 
-
-            <div class="px-4 text-right max-w">
-                <x-add-new-game-button/>
-            </div>
+        <div class="px-4 text-right max-w">
+            <x-add-new-game-button/>
+        </div>
+        <div class="grid grid-cols-3 gap-4 justify-items-stretch">
             @foreach($user->games as $game)
-            <div class="py-6 mx-2 inline-block w-1/3">
-                <div class="flex max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
+                <div class=" flex max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
                     <div class="w-1/3 max-h-40 bg-cover" style="background-image: url('{{ $game->image_url }}')">
                     </div>
                     <div class="w-2/3 p-4">
@@ -19,36 +18,42 @@
                             @for ($i = 0; $i < 5; $i++)
                                 @if ($i < $game->rating)
                                     <svg class="w-5 h-5 fill-current text-gray-700" viewBox="0 0 24 24">
-                                        <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
+                                        <path
+                                            d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
                                     </svg>
                                 @else
                                     <svg class="w-5 h-5 fill-current text-gray-500" viewBox="0 0 24 24">
-                                        <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
+                                        <path
+                                            d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
                                     </svg>
                                 @endif
                             @endfor
                         </div>
                         @if($game->metacritic_rating)
-                        <div class="mx-1">
-                            Metacritic rating: {{ $game->metacritic_rating }} / 100
-                        </div>
+                            <div class="mx-1">
+                                Metacritic rating: {{ $game->metacritic_rating }} / 100
+                            </div>
                         @endif
                         <div class="mt-3">
                             @if(isset($game->metadata['platforms']))
                                 @foreach($game->metadata['platforms'] as $platform)
-                                    <div class="inline-block px-3 py-2 my-1 bg-gray-800 text-white text-xs font-bold uppercase rounded">
+                                    <div
+                                        class="inline-block px-3 py-2 my-1 bg-gray-800 text-white text-xs font-bold uppercase rounded">
                                         {{ $platform['name'] }}</div>
                                 @endforeach
                             @endif
 
                         </div>
                         <div class="flex item-center justify-between mt-3">
-                            <button class="px-3 py-2 bg-purple-800 text-white text-xs font-bold uppercase rounded">Play game</button>
+                            <button class="px-3 py-2 bg-purple-800 text-white text-xs font-bold uppercase rounded">
+                                Play game
+                            </button>
                         </div>
                     </div>
                 </div>
-            </div>
             @endforeach
+        </div>
+
 
     @else
         <div class="text-center">
